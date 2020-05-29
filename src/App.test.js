1da8df12d86d2d6371b6dbd616fb3b7a76ccc3ba
Juan David Nunez <juandavidnh93@gmail.com>
2020-05-29 18:50:45 -0500

    app test passing

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom'
 import App from './App';
 
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(
-    <App />, div);
+    <BrowserRouter>
+    <App /></BrowserRouter>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
